941663731f49e0bfe15d6957b67df8ad0bb6fc89
Thimo <wolfft@hva.nl>
2023-02-05 16:16:49 +0100

    Stap 4

---
+++ Networking/JsonObjects/NoChangeMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Networking.JsonObjects
+{
+    internal class NoChangeMessage : MessagePacket
+    {
+        public uint tickNumber;
+        public sbyte direction;
+
+        public NoChangeMessage() 
+        {
+        
+        }
+    }
+}
